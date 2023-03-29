04112e8cb197df19bab2281710260b42901b8e59
Ian Iversen <ianiversen1@gmail.com>
2023-03-28 22:43:09 -0400

    done: range error

--- errors-overview.js
+++ errors-overview.js
@@ -20,13 +20,34 @@
 
 //type error
 
-let x = 'ian'
+// let x = 'ian'
 
-let y = 1
+// let y = 1
 
 
-console.log(x.toUpperCase());
+// console.log(x.toUpperCase());
 
-console.log(y.toUpperCase())
+// console.log(y.toUpperCase())
 
 // console.log(z.length);
+
+
+
+//range error
+
+// let arr = ['a', 'b', 'c'];
+
+// console.log(arr[3]);
+
+// this throws undefined
+
+// let arr1 = new Array(2)
+
+// console.log(arr1);
+
+// let arr2 = new Array(-1);
+
+// console.log(arr2);
+
+//this last one will throw a range error
+
